438443ef047fa8ae885cc05073cfd5472cb73d82
Charles <dailiang18bb@gmail.com>
2020-07-13 14:46:05 -0400

    Pactice mutiple property files. Compare to mutiple-property-files original branch.

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,15 +2,21 @@ package guru.springframework.config;
 
 
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"}
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -25,6 +31,16 @@ public class PropertyConfig {
     @Value("${charles.dburl}")
     String url;
 
+    @Value("${charles.jms.username}")
+    String jmsUsername;
+
+    @Value("${charles.jms.password}")
+    String jmsPassoword;
+
+    @Value("${charles.jms.url}")
+    String jmsUrl;
+
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -34,6 +50,16 @@ public class PropertyConfig {
         fakeDataSource.setUrl(url);
         return fakeDataSource;
     }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassoword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
+    }
+
 
     /* Redundant
 
